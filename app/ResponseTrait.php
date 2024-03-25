5c02843ad3a5fff8b0330acaa414109a5272c4d3
esraaalghabra3040 <esraaalghabra3040@gmail.com>
2024-03-25 12:40:44 +0200

    create ResponseTrait

---
+++ app/ResponseTrait.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+trait ResponseTrait
+{
+    public function successResponse($data=null, $message = null, $statusCode = 200){
+        return response()->json([
+            'data' => $data,
+            'message' => $message
+        ], $statusCode);
+    }
+
+    public function failedResponse($message = null, $statusCode = 500){
+        return response()->json([
+            'data' => null,
+            'message' => $message
+        ], $statusCode);
+    }
+}
