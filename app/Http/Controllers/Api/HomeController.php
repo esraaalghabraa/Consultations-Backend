ff1c1bf2228b5e38341b6d7b9fe1e9ebd2dfb7f8
esraaalghabra3040 <esraaalghabra3040@gmail.com>
2024-03-25 21:32:37 +0200

    add operations in HomeController

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -9,6 +9,7 @@ use App\Models\SubCategory;
 use App\ResponseTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+
 class HomeController extends Controller
 {
     use ResponseTrait;
@@ -29,12 +30,30 @@ class HomeController extends Controller
         return $this->successResponse($highestRecommendedExperts);
     }
 
+    public function getAllRecommendedExperts()
+    {
+        $highestRecommendedExperts = Expert::
+        where('recommended_number', '>', 0)
+            ->orderByDesc('recommended_number')
+            ->get();
+        return $this->successResponse($highestRecommendedExperts);
+    }
+
     public function getTopExperts()
     {
         $highestRatedExperts = Expert::
-        where('rating_number', '>', 0)
+        where('rating_number', '>', 2)
             ->orderByDesc('rating')
             ->limit(10)
+            ->get();
+        return $this->successResponse($highestRatedExperts);
+    }
+
+    public function getAllTopExperts()
+    {
+        $highestRatedExperts = Expert::
+        where('rating_number', '>', 2)
+            ->orderByDesc('rating')
             ->get();
         return $this->successResponse($highestRatedExperts);
     }
@@ -73,4 +92,201 @@ class HomeController extends Controller
 
     }
 
+    public function search(Request $request){
+        $validator = Validator::make($request->all(), [
+            'query' => ['required', 'string','max:255'],
+        ]);
+        if ($validator->fails())
+            return $this->failedResponse($validator->errors()->first());
+
+        $searchQuery = $request->input('query');
+
+        // Search for experts based on department name or expertise name
+        $experts = Expert::where('full_name', 'like', '%' . $searchQuery . '%')
+            ->orWhereHas('category', function ($query) use ($searchQuery) {
+            $query->where('name', 'like', '%' . $searchQuery . '%');
+        })->orWhereHas('subCategories', function ($query) use ($searchQuery) {
+            $query->where('name', 'like', '%' . $searchQuery . '%');
+        })->orWhereHas('experiences', function ($query) use ($searchQuery) {
+            $query->where('name', 'like', '%' . $searchQuery . '%');
+        })
+            ->get();
+
+        return $this->successResponse($experts);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        $new_array = [
+//[
+//            "id"
+//=>
+//1,
+//"full_name"
+//=>
+//"Alaina Breanna",
+//"phone"
+//=>
+//"+9633477323",
+//"email"
+//=>
+//"sabshire@example.net",
+//"password"
+//=>
+//"$2y$12$5DBcl18t.fWHHMnYUnZ1fe9t5DD7CYOP7saKDBnapULwPp8BuqIHW",
+//"address"
+//=>
+//"Sierra Leone",
+//"about"
+//=>
+//"Sit et quam inventore qui.",
+//"rating"
+//=>
+//2,
+//"rating_number"
+//=>
+//0,
+//"recommended_number"
+//=>
+//0,
+//"consultancies_number"
+//=>
+//0,
+//"min_range"
+//=>
+//6,
+//"max_range"
+//=>
+//50,
+//"category_id"
+//=>
+//1,
+//"deleted_at"
+//=>
+//null,
+//"created_at"
+//=>
+//"2024-03-25T10:25:46.000000Z",
+//"updated_at"
+//=>
+//"2024-03-25T10:25:46.000000Z",],
+//[
+//            "id"
+//=>
+//1,
+//"full_name"
+//=>
+//"Alaina Breanna",
+//"phone"
+//=>
+//"+9633477323",
+//"email"
+//=>
+//"sabshire@example.net",
+//"password"
+//=>
+//"$2y$12$5DBcl18t.fWHHMnYUnZ1fe9t5DD7CYOP7saKDBnapULwPp8BuqIHW",
+//"address"
+//=>
+//"Sierra Leone",
+//"about"
+//=>
+//"Sit et quam inventore qui.",
+//"rating"
+//=>
+//2,
+//"rating_number"
+//=>
+//0,
+//"recommended_number"
+//=>
+//0,
+//"consultancies_number"
+//=>
+//0,
+//"min_range"
+//=>
+//6,
+//"max_range"
+//=>
+//50,
+//"category_id"
+//=>
+//1,
+//"deleted_at"
+//=>
+//null,
+//"created_at"
+//=>
+//"2024-03-25T10:25:46.000000Z",
+//"updated_at"
+//=>
+//"2024-03-25T10:25:46.000000Z",],
+//[
+//            "id"
+//=>
+//1,
+//"full_name"
+//=>
+//"Alaina Breanna",
+//"phone"
+//=>
+//"+9633477323",
+//"email"
+//=>
+//"sabshire@example.net",
+//"password"
+//=>
+//"$2y$12$5DBcl18t.fWHHMnYUnZ1fe9t5DD7CYOP7saKDBnapULwPp8BuqIHW",
+//"address"
+//=>
+//"Sierra Leone",
+//"about"
+//=>
+//"Sit et quam inventore qui.",
+//"rating"
+//=>
+//2,
+//"rating_number"
+//=>
+//0,
+//"recommended_number"
+//=>
+//0,
+//"consultancies_number"
+//=>
+//0,
+//"min_range"
+//=>
+//6,
+//"max_range"
+//=>
+//50,
+//"category_id"
+//=>
+//1,
+//"deleted_at"
+//=>
+//null,
+//"created_at"
+//=>
+//"2024-03-25T10:25:46.000000Z",
+//"updated_at"
+//=>
+//"2024-03-25T10:25:46.000000Z",],
+//        ];
+
