bf9a133367e30e3012054d56b2913995d2fb485c
esraaalghabra3040 <esraaalghabra3040@gmail.com>
2024-06-28 19:35:33 +0200

    Add comments to AuthController controller and aply SOLD principles

--- app/Http/Controllers/Api/V1/Guest/AuthController.php
+++ app/Http/Controllers/Api/V1/Guest/AuthController.php
@@ -2,140 +2,175 @@
 
 namespace App\Http\Controllers\Api\V1\Guest;
 
-use App\Mail\EmailVerification;
-use App\Models\Role;
+use App\Exceptions\AuthenticationException;
+use App\Exceptions\EmailVerificationException;
+use App\Exceptions\OTPException;
 use App\Models\User;
 use App\ResponseTrait;
+use App\Services\AuthService;
+use App\Services\OTPService;
+use App\Services\RoleService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     use ResponseTrait;
 
-    public function __construct()
+    protected AuthService $authService;
+    protected OTPService $otpService;
+    protected RoleService $roleService;
+
+    public function __construct(AuthService $authService, OTPService $otpService, RoleService $roleService)
     {
-        $this->middleware(['auth:sanctum', 'abilities:user,access'])
-            ->except(['register', 'sendCode', 'verifiedEmail', 'login', 'refreshToken', 'getUser']);
+        $this->authService = $authService;
+        $this->otpService = $otpService;
+        $this->roleService = $roleService;
+
+        // Applying middleware to specific methods
+        $this->middleware(['auth:sanctum', 'abilities:user,access'])->only(['resetPassword']);
         $this->middleware(['auth:sanctum', 'ability:user,refresh'])->only('refreshToken');
     }
 
-    public function register(Request $request)
+    // Method for registering a new user
+    public function register(Request $request): JsonResponse
     {
+        // Validate the incoming request data
         $validator = Validator::make($request->all(), [
-            'first_name' => ['required', 'string', 'min:6', 'max:20'],
-            'last_name' => ['required', 'string', 'min:6', 'max:20'],
+            'first_name' => ['required', 'string', 'min:3', 'max:20'],
+            'last_name' => ['required', 'string', 'min:3', 'max:20'],
             'email' => ['required', 'string', 'email', 'unique:users,email'],
             'password' => ['required', 'string', 'min:6', 'max:20'],
             'is_expert' => ['required', 'boolean']
         ]);
         if ($validator->fails())
             return $this->failedResponse($validator->errors()->first());
-        $otp = mt_rand(100000, 999999);
-        $details = ['full_name' => $request->first_name, 'otp' => $otp];
+
+        // Register the user and assign a role
+        $user = $this->authService->register($request->all());
+        $role = $request->is_expert ? 'expert' : 'customer';
+        $this->roleService->assignRole($user, $role);
+
+        // Send OTP code to the user
+        return $this->sendCode($user);
+    }
+
+    // Method for sending OTP code
+    public function sendCode(User $user): JsonResponse
+    {
         try {
-            Mail::to($request->email)->send(new EmailVerification($details));
-        } catch (\Exception $exception) {
-            return $this->failedResponse();
+            $this->otpService->sendCode($user);
+        } catch (OTPException|EmailVerificationException $e) {
+            return $this->failedResponse($e->getMessage());
+        } catch (\Exception $e) {
+            return $this->failedResponse('An unexpected error occurred');
         }
-        $user = User::create([
-            'full_name' => $request->full_name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'otp' => $otp
-        ]);
-        if ($request->is_expert)
-            $role = Role::where('name', 'expert')->first();
-        else
-            $role = Role::where('name', 'customer')->first();
-        $user->addRole($role);
+
         return $this->successResponse();
     }
 
-    public function login(Request $request)
+    // Method for logging in a user
+    public function login(Request $request): JsonResponse
     {
+        // Validate the incoming request data
         $validator = Validator::make($request->all(), [
             'email' => ['string', 'exists:users,email,deleted_at,NULL'],
             'password' => ['required', 'string', 'min:6', 'max:20']
         ]);
         if ($validator->fails())
             return $this->failedResponse($validator->errors()->first());
-        if (!auth()->validate($request->only('password', 'email'))) {
-            return $this->failedResponse('The provided credentials are incorrect.');
+
+        try {
+            // Attempt to log in the user
+            $user = $this->authService->login($request->only('email', 'password'));
+        } catch (AuthenticationException $e) {
+            return $this->failedResponse($e->getMessage());
+        } catch (\Exception $e) {
+            return $this->failedResponse('An unexpected error occurred');
         }
-        $user = User::where('email', $request->email)->first();
+
+        // Return the logged-in user details
         return $this->getUser($user);
     }
 
-    public function sendCode(Request $request)
+    // Method for getting user details
+    public function getUser($user): JsonResponse
     {
+        // Attach role and generate tokens for the user
+        $user->role = $user->roles[0]['name'];
+        Arr::forget($user, 'roles');
+        $user->token = $user->createToken('accessToken', ['user', 'access'], now()->addDays(6))->plainTextToken;
+        $user->refresh_token = $user->createToken('refreshToken', ['user', 'refresh'], now()->addDays(12))->plainTextToken;
+
+        // Return success response with user details
+        return $this->successResponse($user);
+    }
+
+    // Method for resending OTP code
+    public function reSendCode(Request $request): JsonResponse
+    {
+        // Validate the incoming request data
         $validator = Validator::make($request->all(), [
             'email' => ['required', 'string', 'email', 'exists:users,email,deleted_at,NULL'],
         ]);
-        if ($validator->fails())
+
+        if ($validator->fails()) {
             return $this->failedResponse($validator->errors()->first());
+        }
+
+        // Find the user by email and resend the OTP code
         $user = User::where('email', $request->email)->first();
-        $currentTime = now();
-        // Check if the user is allowed to request a new OTP
-        if ($user->otp_last_sent_at && $currentTime->lessThan($this->calculateNextAllowedTime($user))) {
-            return $this->failedResponse('You must wait before requesting a new OTP');
-        }
-        // Generate OTP
-        $otp = mt_rand(100000, 999999);
-        $details = ['full_name' => $user->first_name, 'otp' => $otp];
-        try {
-            Mail::to($request->email)->send(new EmailVerification($details));
-        } catch (\Exception $exception) {
-            return $this->failedResponse();
-        }
-        $user->update([
-            'otp' => $otp,
-            'otp_last_sent_at' => $currentTime,
-            'otp_resend_count' => $user->otp_resend_count + 1,
-        ]);
-        $user->save();
-        return $this->successResponse();
+        return $this->sendCode($user);
     }
 
-    public function verifiedEmail(Request $request)
+    // Method for verifying email with OTP code
+    public function verifiedEmail(Request $request): JsonResponse
     {
+        // Validate the incoming request data
         $validator = Validator::make($request->all(), [
             'email' => ['required', 'string', 'exists:users,email,deleted_at,NULL'],
-            'code' => ['required', 'numeric']
+            'otp' => ['required', 'numeric']
         ]);
         if ($validator->fails())
             return $this->failedResponse($validator->errors()->first());
-        $user = User::where('email', $request->email)->where('otp', $request->otp)->first();
-        if ($user->otp_last_sent_at < now()->subMinutes(15)) {
-            $user->otp = null;
-            return $this->failedResponse('Invalid code');
+
+        // Find the user by email and verify the OTP code
+        $user = User::where('email', $request->email)->first();
+        try {
+            $this->otpService->verifyCode($user, $request->otp);
+        } catch (OTPException $e) {
+            return $this->failedResponse($e->getMessage());
         }
         if (!$user->markEmailAsVerified()) {
             $user->markEmailAsVerified();
-            $user->otp = null;
-            $user->save();
         }
         return $this->getUser($user);
     }
 
+    // Method for resetting the password
     public function resetPassword(Request $request)
     {
-        $user = Auth::user();
-        $user->update([
-            'password' => Hash::make($request->password)
+        // Validate the incoming request data
+        $validator = Validator::make($request->all(), [
+            'password' => ['required', 'string', 'min:6', 'max:20']
         ]);
-        $user->save();
+        if ($validator->fails()) {
+            return $this->failedResponse($validator->errors()->first());
+        }
+
+        // Reset the user's password
+        $this->authService->resetPassword(Auth::user(), $request->password);
         return $this->successResponse();
     }
 
     public function refreshToken(Request $request)
     {
         try {
+            // Delete old tokens and create new ones
             $request->user()->tokens()->delete();
             $token = $request->user()->createToken('accessToken', ['user', 'access'], now()->addDay())->plainTextToken;
             $r_token = $request->user()->createToken('refreshToken', ['user', 'refresh'], now()->addDays(6))->plainTextToken;
@@ -145,42 +180,11 @@ class AuthController extends Controller
         }
     }
 
-    public function logout()
+    // Method for logging out the user
+    public function logout(): JsonResponse
     {
+        // Delete the current access token of the user
         Auth::user()->currentAccessToken()->delete();
         return $this->successResponse();
-    }
-
-    private function calculateNextAllowedTime(User $user)
-    {
-        $resendCount = $user->otp_resend_count;
-
-        switch ($resendCount) {
-            case 1:
-                return now()->addMinute();
-            case 2:
-                return now()->addMinutes(5);
-            case 3:
-                return now()->addMinutes(15);
-            case 4:
-                return now()->addMinutes(30);
-            case 5:
-                return now()->addHour();
-            default:
-                return now()->addDay();
-        }
-    }
-
-    /**
-     * @param $user
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function getUser($user): \Illuminate\Http\JsonResponse
-    {
-        $user->role = $user->roles[0]['name'];
-        Arr::forget($user, 'roles');
-        $user->token = $user->createToken('accessToken', ['user', 'access'], now()->addDays(6))->plainTextToken;
-        $user->refresh_token = $user->createToken('refreshToken', ['user', 'refresh'], now()->addDays(12))->plainTextToken;
-        return $this->successResponse($user);
     }
 }
