fbb1ace9236944e4a96c9effc516e4371bd298d8
esraaalghabra3040 <esraaalghabra3040@gmail.com>
2024-06-28 19:08:58 +0200

    some columns drop from experts table and build relationship between expert and user and followers

--- database/migrations/2024_03_17_141941_create_experts_table.php
+++ database/migrations/2024_03_17_141941_create_experts_table.php
@@ -13,22 +13,16 @@ return new class extends Migration
     {
         Schema::create('experts', function (Blueprint $table) {
             $table->id();
-            $table->string('full_name');
-            $table->string('email')->unique();
-            $table->string('phone');
-            $table->string('password');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('category_id')->nullable()->constrained('categories')->onDelete('cascade');
+            $table->string('about')->nullable();
             $table->string('address')->nullable();
-            $table->string('gender')->nullable();
-            $table->date('birthdate')->nullable();
-            $table->text('about')->nullable();
-            $table->integer('otp')->nullable();
-            $table->boolean('is_complete_data')->default(0);
-            $table->timestamp('email_verified_at')->nullable();
             $table->double('rating')->default(0.0);
             $table->integer('rating_number')->default(0);
             $table->integer('recommended_number')->default(0);
             $table->bigInteger('consultancies_number')->default(0);
-            $table->foreignId('category_id')->nullable()->constrained('categories')->onDelete('cascade');
+            $table->double('min_cost')->nullable();
+            $table->double('max_cost')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
