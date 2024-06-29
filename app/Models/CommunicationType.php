ea672bd83c32881c5a8a089018654325fc2c415f
esraaalghabra3040 <esraaalghabra3040@gmail.com>
2024-06-29 22:49:14 +0200

    hide 'deleted_at', 'created_at', 'updated_at' columns from CommunicationType

--- app/Models/CommunicationType.php
+++ app/Models/CommunicationType.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CommunicationType extends Model
@@ -11,10 +12,10 @@ class CommunicationType extends Model
     use HasFactory, SoftDeletes;
 
     protected $guarded = [];
-    protected $hidden = ['pivot'];
+    protected $hidden = ['pivot','deleted_at', 'created_at', 'updated_at'];
 
-    public function experts()
+    public function experts(): BelongsToMany
     {
-        return $this->belongsToMany(Expert::class, 'expert_experiences');
+        return $this->belongsToMany(Expert::class, 'communication_type_expert');
     }
 }
