2644379877385a0b897ed4190b811b8e4e944113
esraaalghabra3040 <esraaalghabra3040@gmail.com>
2024-06-29 23:08:52 +0200

    update endpoints

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::controller(HomeController::class)->group(function (){
     Route::get('get_home','getHomeData');
     Route::get('get_experts','getExperts');
-    Route::get('get_main_category_details','getMainCategoryDetails');
-    Route::get('get_expert_details','getExpertDetails');
-    Route::get('search','search');
 });
 require __DIR__.'/auth.php';
 require __DIR__.'/customer.php';
--- routes/expert.php
+++ routes/expert.php
@@ -1,27 +1,19 @@
 <?php
 
-use App\Http\Controllers\Api\V1\Expert\AuthExpertController;
 use App\Http\Controllers\Api\V1\Expert\ExpertController;
-use App\Http\Controllers\Api\V1\Expert\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::controller(AuthExpertController::class)->group(function () {
-    Route::post('register', 'register');
-    Route::post('login', 'login');
-    Route::post('send_code', 'sendCode');
-    Route::post('verified_email', 'verifiedEmail');
-    Route::post('reset_password', 'resetPassword');
-    Route::get('logout', 'logout');
-
-});
-Route::middleware(['auth:sanctum', 'abilities:expert,access'])->controller(ExpertController::class)->group(function () {
+Route::prefix('expert')
+    ->middleware(['auth:sanctum', 'abilities:user,access','role_expert'])
+    ->controller(ExpertController::class)->group(function () {
+    Route::get('get_categories', 'getCategories');
     Route::get('get_profile', 'getProfile');
     Route::post('complete_information', 'completeInfo');
 });
-Route::middleware(['auth:sanctum', 'abilities:expert,access'])->controller(UserController::class)->group(function () {
-    Route::get('get_appointments', 'getAppointments');
-    Route::post('accept_appointment', 'acceptAppointment');
-    Route::post('response_appointment', 'responseAppointment');
-
-});
+//Route::middleware(['auth:sanctum', 'abilities:expert,access'])->controller(UserController::class)->group(function () {
+//    Route::get('get_appointments', 'getAppointments');
+//    Route::post('accept_appointment', 'acceptAppointment');
+//    Route::post('response_appointment', 'responseAppointment');
+//
+//});
