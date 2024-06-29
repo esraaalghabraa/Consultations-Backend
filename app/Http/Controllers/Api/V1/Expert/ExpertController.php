d6fc9b7290e76338ae66dde72084549f68ce82e8
esraaalghabra3040 <esraaalghabra3040@gmail.com>
2024-06-29 23:07:33 +0200

    Add comments to ExpertController controller and apply SOLD principles

--- app/Http/Controllers/Api/V1/Expert/ExpertController.php
+++ app/Http/Controllers/Api/V1/Expert/ExpertController.php
@@ -2,156 +2,128 @@
 
 namespace App\Http\Controllers\Api\V1\Expert;
 
+use App\Exceptions\ExistObjectException;
 use App\Http\Controllers\Controller;
-use App\Models\Category;
-use App\Models\CommunicationType;
-use App\Models\Day;
-use App\Models\Expert;
-use App\Models\ExpertCommunications;
-use App\Models\ExpertDate;
-use App\Models\ExpertExperience;
-use App\Models\Hour;
-use App\Models\SubCategory;
-use App\Models\SubCategoryExpert;
-use App\Models\WorkTime;
+use App\ImageTrait;
 use App\ResponseTrait;
+use App\Services\CategoryService;
+use App\Services\CommunicationService;
+use App\Services\ExpertService;
+use App\Services\SubCategoryService;
+use App\Services\UserService;
+use App\Services\WorkTimeService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class ExpertController extends Controller
 {
-    use ResponseTrait;
+    use ResponseTrait, ImageTrait;
 
-    public function __construct()
+    protected ExpertService $expertService;
+    protected CategoryService $categoryService;
+    protected UserService $userService;
+    protected WorkTimeService $workTimeService;
+
+    protected CommunicationService $communicationService;
+    protected SubCategoryService $subCategoryService;
+
+    /**
+     * Constructor for injecting dependencies
+     */
+    public function __construct(WorkTimeService      $workTimeService, UserService $userService,
+                                CategoryService      $categoryService, ExpertService $expertService,
+                                CommunicationService $communicationService, SubCategoryService $subCategoryService)
     {
-        $this->middleware(['auth:sanctum', 'abilities:expert,access']);
+        // Middleware for authentication and role checks
+        $this->middleware(['auth:sanctum', 'abilities:user,access', 'role_expert']);
+
+        // Assigning services to properties
+        $this->userService = $userService;
+        $this->categoryService = $categoryService;
+        $this->expertService = $expertService;
+        $this->workTimeService = $workTimeService;
+        $this->communicationService = $communicationService;
+        $this->subCategoryService = $subCategoryService;
     }
 
-    public function getSubCategoriesWithExperiences(Request $request)
+    /**
+     * Method to get categories with their subcategories
+     */
+    public function getCategories()
     {
-        $validator = Validator::make($request->all(), [
-            'category_id' => ['required', 'exists:categories,id,deleted_at,NULL'],
-        ]);
-        if ($validator->fails())
-            return $this->failedResponse($validator->errors()->first());
-        $sub_categories = SubCategory::query()->where('category_id', $request->category_id);
-
-        $sub_categories_list = $sub_categories->get();
-        $sub_categories_with_experiences = $sub_categories->with('experiences')->get();
-        $experiences_list = $sub_categories_with_experiences->flatMap(function ($element) {
-            return $element->experiences;
-        });
-        return $this->successResponse([
-            'sub_categories' => $sub_categories_list,
-            'experiences' => $experiences_list,
-        ]);
+        return $this->categoryService->getCategoriesWithSubcategories();
     }
 
-    public function getDaysAndHours()
-    {
-        $hours = Hour::get();
-        $days = Day::get();
-        $categories = Category::get();
-        $communication_types = CommunicationType::get();
-        return $this->successResponse([
-            'hours' => $hours,
-            'days' => $days,
-            'categories' => $categories,
-            'communication_types' => $communication_types,
-        ]);
-    }
-
-
+    /**
+     * Method to get the profile of the authenticated expert
+     */
     public function getProfile()
     {
-        $expert = Expert::with('subCategories')
-            ->with('expertDates')->with('expertDates.hour')->with('expertDates.day')
-            ->with('experiences')
-            ->with('communicationTypes')
-            ->with('users')
-            ->with('followers')
-            ->find(Auth::user()->id);
-
-        return $this->successResponse($expert);
+        return $this->successResponse($this->expertService->getExpert(Auth::user()->id));
     }
 
+    /**
+     * Method to complete the expert's information
+     */
     public function completeInfo(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'address' => ['required', 'string', 'max:255'],
-            'gender' => ['required', 'string'],
-            'birthdate' => ['required', 'date'],
-            'about' => ['required', 'string'],
-            'category_id' => ['required', 'exists:categories,id,deleted_at,NULL'],
-            'work_times' => ['required', 'array', 'min:1'],
-            'work_times.*.day_id' => ['required', 'exists:days,id,deleted_at,NULL'],
-            'work_times.*.start_time_id' => ['required', 'exists:hours,id,deleted_at,NULL'],
-            'work_times.*.end_time_id' => ['required', 'exists:hours,id,deleted_at,NULL'],
-            'work_times.*.date' => ['required', 'date'],
-            'experiences' => ['required', 'array', 'min:1'],
-            'experiences.*.id' => ['required', 'exists:experiences,id,deleted_at,NULL'],
-            'experiences.*.experience_years' => ['required', 'numeric'],
-            'communication_types' => ['required', 'array', 'min:1'],
-            'communication_types.*.id' => ['required', 'exists:communication_types,id,deleted_at,NULL'],
-            'communication_types.*.cost_appointment' => ['required', 'numeric'],
-            'sub_category_ids' => ['required', 'array', 'min:1'],
-            'sub_category_ids.*' => ['required', 'exists:sub_categories,id,deleted_at,NULL'],
-        ]);
+        // Validate the request data
+        $validator = $this->validateRequest($request);
+
         if ($validator->fails())
             return $this->failedResponse($validator->errors()->first());
 
-        $expert = Expert::find(Auth::user()->id);
-        $expert->update([
-            'address' => $request->address,
-            'gender' => $request->gender,
-            'birthdate' => $request->birthdate,
-            'about' => $request->about,
-            'category_id' => $request->category_id,
-            'is_complete_data' => 1,
-        ]);
-        $expert->save();
-        foreach ($request->work_times as $item) {
-            WorkTime::create([
-                'expert_id' => $expert->id,
-                'day_id' => $item['day_id'],
-                'start_time_id' => $item['start_time_id'],
-                'end_time_id' => $item['end_time_id'],
-                'date' => $item['date'],
-            ]);
-            for ($i = $item['start_time_id']; $i <= $item['end_time_id']; $i++) {
-                ExpertDate::create([
-                    'expert_id' => $expert->id,
-                    'hour_id' => $i,
-                    'day_id' => $item['day_id'],
-                ]);
-            }
-        }
-        foreach ($request->experiences as $item) {
-            ExpertExperience::create([
-                'expert_id' => $expert->id,
-                'experience_years' => $item['experience_years'],
-                'experience_id' => $item['id'],
-            ]);
-        }
-        foreach ($request->communication_types as $item) {
-            $communication = CommunicationType::find($item['id']);
-            ExpertCommunications::create([
-                'expert_id' => $expert->id,
-                'cost_appointment' => $communication->cost + $item['cost_appointment'],
-                'communication_type_id' => $item['id'],
-            ]);
-        }
-        foreach ($request->sub_category_ids as $item) {
-
-            SubCategoryExpert::create([
-                'expert_id' => $expert->id,
-                'sub_category_id' => $item,
-            ]);
+        $image = null;
+        if ($request->has('image')) {
+            $image = $this->setImage($request->file('image'));
         }
 
+        try {
+            // Get authenticated user and expert
+            $user = $this->userService->getAuthenticatedUser();
+            $expert = $this->userService->getAuthenticatedExpert();
+
+            // Update user and expert information
+            $this->userService->updateUser($user, $request, $image);
+            $this->expertService->updateExpert($expert, $request);
+
+            // Create work times, communication types, and subcategories for the expert
+            $this->workTimeService->createWorkTimesToExpert($expert, $request->work_times);
+            $this->communicationService->addCommunicationTypesToExpert($expert, $request->communication_types_ids);
+            $this->subCategoryService->addSubCategoriesToExpert($expert, $request->sub_category_ids);
+        } catch (ExistObjectException $e) {
+            // Handle known exceptions
+            return $this->failedResponse($e->getMessage());
+        } catch (\Exception $e) {
+            // Handle unexpected exceptions
+            return $this->failedResponse('An unexpected error occurred');
+        }
         return $this->successResponse($expert);
     }
 
-
+    /**
+     * Method to validate the request data
+     */
+    private function validateRequest(Request $request)
+    {
+        return Validator::make($request->all(), [
+            'image' => ['image', 'mimes:jpeg,jpg,png,svg|max:255'],
+            'about' => ['required', 'string'],
+            'address' => ['required', 'string', 'max:255'],
+            'birthdate' => ['required', 'date', 'before:today', 'date_format:Y-m-d'],
+            'gender' => ['required', 'string'],
+            'min_cost' => ['required', 'numeric'],
+            'max_cost' => ['required', 'numeric', 'gt:min_cost'],
+            'category_id' => ['required', 'exists:categories,id,deleted_at,NULL'],
+            'communication_types_ids' => ['required', 'array', 'min:1'],
+            'communication_types.*' => ['required', 'exists:communication_types,id,deleted_at,NULL'],
+            'sub_category_ids' => ['required', 'array', 'min:1'],
+            'sub_category_ids.*' => ['required', 'exists:sub_categories,id,deleted_at,NULL'],
+            'work_times' => ['required', 'array', 'min:1'],
+            'work_times.*.day' => ['required', 'string', 'exists:days,name,deleted_at,NULL'],
+            'work_times.*.start_time' => ['required', 'date_format:g,h:i A'],
+            'work_times.*.end_time' => ['required', 'date_format:g,h:i A', 'after:start'],
+        ]);
+    }
 }
